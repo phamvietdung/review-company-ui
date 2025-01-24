7e8a07cb34e1a6759ca0839a634e28141f56af4f
duvie <dungpv.khtn@gmail.com>
2025-01-25 01:09:44 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { memo, useState } from 'react'
+import { memo, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,6 +6,7 @@ import '@mantine/core/styles.css';
 import { ActionIcon, Button, Group, MantineProvider, Switch, TextInput, useMantineColorScheme, useMantineTheme } from '@mantine/core';
 import { Container, Grid, Skeleton, Table, TableData } from '@mantine/core';
 import { IconMoonFilled, IconSun, IconArrowRight, IconSearch } from '@tabler/icons-react'
+import axios from 'axios';
 
 const child = <Skeleton style={{
     background: "#ccc", marginTop: 5
@@ -15,9 +16,15 @@ const tableData: TableData = {
     // caption: 'Some elements from periodic table',
     head: ['Tên Công Ty', 'Lương', 'Khác', 'Năm'],
     body: [
-        ["Công ty 1", "10.000.000", "Không", "2021"],
+
     ],
 };
+
+const instance = axios.create({
+    baseURL: 'http://127.0.0.1:8000',
+    timeout: 2000,
+    // headers: {'X-Custom-Header': 'foobar'}
+});
 
 
 function App() {
@@ -25,6 +32,25 @@ function App() {
     const { colorScheme, toggleColorScheme } = useMantineColorScheme()
 
     const theme = useMantineTheme();
+
+    const [bodyData, setBodyData] = useState<any[]>([])
+
+    const [search, setSearch] = useState<string>("")
+
+    const fetchData = (search_key: string = "") => {
+        instance.post("/search", {
+            "search_text": search_key,
+            "page": 0,
+            "page_size": 20
+        }).then((success: any) => {
+            tableData.body?.push(["Công ty 1", "10.000.000", "Không", "20212"])
+
+        })
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
 
     return (
         <>
@@ -42,21 +68,23 @@ function App() {
                         </Group>
 
                         <TextInput
+                            value={search}
                             style={{ marginTop: 20 }}
                             radius="xl"
                             size="md"
                             placeholder="Tìm kiếm công ty"
                             rightSectionWidth={42}
                             leftSection={<IconSearch size={18} stroke={1.5} />}
+                            onChange={e => setSearch(e.target.value)}
                             onKeyDown={(e) => {
                                 if (e.key === 'Enter') {
-                                    console.log('search')
+                                    fetchData(search)
                                 }
                             }}
                             rightSection={
                                 <ActionIcon size={32} radius="xl" color={theme.primaryColor} variant="filled"
 
-                                    onClick={() => { console.log('search') }}
+                                    onClick={() => { fetchData(search) }}
                                 >
                                     <IconArrowRight size={18} stroke={1.5} />
                                 </ActionIcon>
@@ -65,7 +93,10 @@ function App() {
                         />
 
                         <div style={{ marginTop: 20, padding: 10, borderRadius: 10, border: "1px solid #ddd" }}>
-                            <Table data={tableData} />
+                            <Table data={{
+                                head: ['Tên Công Ty', 'Lương', 'Khác', 'Năm'],
+                                body: bodyData
+                            }} />
                         </div>
 
 
